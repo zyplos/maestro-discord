1395436f996a4c80507c93b682a53949a89d1d3d
Angel <22824872+Zyplos@users.noreply.github.com>
2023-02-17 21:08:22 -0600

    member bans now recorded

---
+++ src/events/guildBanAdd.ts
@@ -0,0 +1,29 @@
+import { Client, EmbedBuilder, GuildBan } from "discord.js";
+import { getGuildLogChannel } from "../internals/util";
+
+module.exports = async (client: Client, ban: GuildBan) => {
+  client.logger.debug(`${ban.user.tag} was banned from ${ban.guild.name}`);
+
+  const logChannel = await getGuildLogChannel(client, ban.guild.id);
+  if (!logChannel) return; // guild hasn't set up their log channel
+
+  console.log("ban", ban);
+
+  const msgEmbed = new EmbedBuilder()
+    .setTitle("Member Banned")
+    .setDescription(`**${ban.user.tag} (${ban.user.id})** was banned from the server.`)
+    .setColor(0x550707)
+    .setTimestamp(new Date())
+    .setThumbnail(
+      ban.user.displayAvatarURL({
+        extension: "png",
+        size: 128,
+      })
+    );
+
+  if (ban.reason) {
+    msgEmbed.addFields({ name: "Reason", value: ban.reason });
+  }
+
+  logChannel.send({ content: "\t", embeds: [msgEmbed] });
+};
---
+++ src/events/guildBanRemove.ts
@@ -0,0 +1,29 @@
+import { Client, EmbedBuilder, GuildBan } from "discord.js";
+import { getGuildLogChannel } from "../internals/util";
+
+module.exports = async (client: Client, ban: GuildBan) => {
+  client.logger.debug(`${ban.user.tag} was banned from ${ban.guild.name}`);
+
+  const logChannel = await getGuildLogChannel(client, ban.guild.id);
+  if (!logChannel) return; // guild hasn't set up their log channel
+
+  console.log("unban", ban);
+
+  const msgEmbed = new EmbedBuilder()
+    .setTitle("Member Unbanned")
+    .setDescription(`**${ban.user.tag} (${ban.user.id})** was unbanned from the server.`)
+    .setColor(0x175507)
+    .setTimestamp(new Date())
+    .setThumbnail(
+      ban.user.displayAvatarURL({
+        extension: "png",
+        size: 128,
+      })
+    );
+
+  if (ban.reason) {
+    msgEmbed.addFields({ name: "Reason", value: ban.reason });
+  }
+
+  logChannel.send({ content: "\t", embeds: [msgEmbed] });
+};
